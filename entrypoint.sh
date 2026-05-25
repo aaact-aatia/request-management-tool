b65c7c86df88b2834dac27ab48d5c95873fa21c2
Shawn Thompson <plansmash@me.com>
2026-05-25 08:42:58 -0400

    build: package app for azure container runtime

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-set -e
+#!/bin/sh
+set -eu
 
-# Install dependencies if vendor/ doesn't exist
-if [ ! -d /var/www/html/vendor ]; then
-  composer install --no-dev --no-interaction --working-dir=/var/www/html
-fi
-
-# Start Apache
 exec docker-php-entrypoint apache2-foreground
