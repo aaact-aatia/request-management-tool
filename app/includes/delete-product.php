463834416c106ac241c076848a582fe18c2d0e91
Shawn Thompson <plansmash@me.com>
2026-09-14 13:30:50 -0400

    fix: harden product administration CRUD

--- app/includes/delete-product.php
+++ app/includes/delete-product.php
@@ -3,6 +3,8 @@
 
 // Start session
 require_once __DIR__ . '/session_start.php';
+require_once('csrf.php');
+require_once('helpers.php');
 
 // Get language
 $lang = $_GET['lang'] ?? 'en';
@@ -17,29 +19,32 @@ if (!($_SESSION['is_superuser'] OR $_SESSION['is_admin'])) {
 require('../sql.php');
 
 // Now first get the ID
-$productid = $_GET['id'];
+$productid = filter_var($_GET['id'] ?? null, FILTER_VALIDATE_INT, ['options' => ['min_range' => 1]]) ?: 0;
+$csrfToken = rmt_csrf_token('products');
 
 // Process the delete product form
 if ($_SERVER['REQUEST_METHOD']=='POST'){
-	
-	// Create SQL statement
-	$sql = "UPDATE `tblproducts` SET `status` = '0' WHERE id='$productid'";
-	//echo $sql;
-	rmt_admin_query($link,$sql);
+	if (!rmt_csrf_token_is_valid('products', (string) ($_POST['csrf_token'] ?? '')) || $productid <= 0) {
+		header("location:/products.php?lang={$lang}&status=failed");
+		exit();
+	}
+
+	$statement = rmt_db_execute($link, 'UPDATE tblproducts SET status = 0 WHERE id = ?', 'i', [$productid]);
+	mysqli_stmt_close($statement);
 	
 	// Now redirect
 	header("location:/products.php?lang=$lang&status=success"); 
 	exit();
 }
 
-// Construct SQL statement
-$sql2 = "SELECT * FROM tblproducts WHERE id='$productid'";
 
-$result2 = rmt_admin_query($link,$sql2);
-//List it
-if(rmt_result_num_rows($result2)>0){
-	while($row2 = rmt_result_fetch_array($result2)){
-		$name = ($lang == 'fr') ? $row2['namefr'] : $row2['nameen'];
+$product = $productid > 0
+	? rmt_db_fetch_one($link, 'SELECT id, nameen, namefr FROM tblproducts WHERE id = ?', 'i', [$productid])
+	: null;
+
+if ($product) {
+	$row2 = $product;
+	$name = ($lang == 'fr') ? $row2['namefr'] : $row2['nameen'];
 		$title = ($lang == 'fr') ? "Supprimer le produit $name" : "Delete $name product";
 		$question = ($lang == 'fr') ? "Êtes-vous sûr de vouloir supprimer ce produit?" : "Are you sure you wish to delete this product?";
 		$buttonText = ($lang == 'fr') ? "Oui" : "Yes";
@@ -50,6 +55,7 @@ if(rmt_result_num_rows($result2)>0){
 	</header>
 	<div class="modal-body">
 		<form method="post" action="/includes/delete-product.php?lang=<?php echo $lang ?>&id=<?php echo $row2['id'] ?>">
+		<input type="hidden" name="csrf_token" value="<?= htmlspecialchars($csrfToken, ENT_QUOTES, 'UTF-8') ?>">
 		<p tabindex="0"><?php echo $question ?></p>
 		<div class="form-group form-buttons">
 			<button type="submit" class="btn btn-default"><?php echo $buttonText ?></button>
@@ -59,8 +65,7 @@ if(rmt_result_num_rows($result2)>0){
 	</div>
 </section>
 <?php
-	}
-} else { 
+} else {
 // Wrong ID so display an error message
 	$errorTitle = ($lang == 'fr') ? "Oups, quelque chose s'est mal passé!" : "Oops something went wrong!";
 	$errorMsg = ($lang == 'fr') ? "Désolé, une erreur s'est produite avec votre demande, veuillez réessayer!" : "Sorry something went wrong with your request, please try again!";
